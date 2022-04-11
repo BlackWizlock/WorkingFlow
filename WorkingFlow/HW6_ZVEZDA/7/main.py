e9a3b405cfff3a254aa8733adf0472073cff600e
BlackWizlock <aa.salikov@mail.ru>
2022-04-11 10:11:46 +0300

    Some commit

--- WorkingFlow/HW6_ZVEZDA/7/main.py
+++ WorkingFlow/HW6_ZVEZDA/7/main.py
@@ -1,9 +1,16 @@
+import re
+
+pattern_phrases = r'[.!?;:]\s+[A-ZА-Я]'
+pattern_words = r'\w+'
+pattern_symbols = r'\S'
+
+
 def read_file(filename: str):
 	lst_file_data = list()
 	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			lst_file_data.append(line.strip())
-	return lst_file_data
+	return " ".join(lst_file_data)
 
 
 def show_statistic(phrase: int = 0, words: int = 0, symbols: int = 0):
@@ -13,21 +20,11 @@ def show_statistic(phrase: int = 0, words: int = 0, symbols: int = 0):
 
 
 def main():
-	# user_file_name = str(input('Введите имя файла :'))
 	user_file_input = read_file('db.txt')
-	words = 0
-	phrases = 0
-	symbols = 0
-	for lines in user_file_input:
-		words += len(lines.split(" "))
-
-	for lines in user_file_input:
-		for symbol in lines:
-			if not symbol.isspace():
-				symbols += 1
-
-	show_statistic(words=words, symbols=symbols)
-	# print(*user_file_input, sep='\n')
+	words = len(re.findall(pattern_words, user_file_input))
+	phrases = len(re.findall(pattern_phrases, user_file_input)) + 1
+	symbols = len(re.findall(pattern_symbols, user_file_input))
+	show_statistic(words=words, symbols=symbols, phrase=phrases)
 
 
 if __name__ == '__main__':
