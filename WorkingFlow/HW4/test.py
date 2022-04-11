66d516132d4d75ea2781d31cd37d7d2ca90424b3
BlackWizlock <aa.salikov@mail.ru>
2022-04-11 11:32:29 +0300

    Some commit

---
+++ WorkingFlow/HW4/test.py
@@ -0,0 +1,24 @@
+import collections
+print(set("7asd7"))
+print(list("1"))
+print(dict(Key1=1))
+print(tuple("1",))
+
+
+print(ord("“"))
+print(ord("”"))
+print(ord("«"))
+print(ord("»"))
+print(ord(" "))
+
+print(f'''
+      
+      {(a := False * 1)}
+      
+      ''')
+
+
+words = collections.defaultdict (int)
+
+words["Test"] += 1
+print (words)
