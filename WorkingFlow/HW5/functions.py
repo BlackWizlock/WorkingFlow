f8552e87d13cdfdf78e9d4be9b39a0762f4be3a5
BlackWizlock <aa.salikov@mail.ru>
2022-04-19 11:54:06 +0300

    Some commit

---
+++ WorkingFlow/HW5/functions.py
@@ -0,0 +1,42 @@
+def shuffle_letters(word):
+    """
+    Функция для перемешивания букв в слове
+    """
+    import random
+    items = random.sample(word, len(word))
+    encoded_word = "".join(items)
+    return encoded_word.replace("\n", "")
+
+
+def get_statistic():
+    """
+    Функция, возвращающая рекорд и количество игр
+    """
+    game_counter = 0
+    get_score = []
+    with open("history.txt", "r") as file:
+        for item in file:
+            game_counter += 1
+            name, score = item.strip().split(" ")
+            get_score.append(score)
+            max_score = max(get_score)
+        return [game_counter, max_score]
+
+
+def print_statistic():
+    """
+    Функция, выводящая на экран рекорд и количество игр
+    """
+    game_statistic, max_statistic = get_statistic()
+    print(f"Всего игр сыграно: {game_statistic}")
+    print(f"Максимальный рекорд: {max_statistic}")
+
+
+def write_to_the_top(name, score):
+    """
+    Функция, записывающая результат в топ
+    """
+    with open("history.txt", "a") as file:
+        file.write(f"{name} {score}\n")
+
+
---
+++ WorkingFlow/HW5/main.py
@@ -0,0 +1,23 @@
+# Это программа помогает учить английский
+
+import functions
+score = 0  # счётчик баллов
+name = input("Введите имя пользователя\n")
+
+with open("words.txt", "r") as file:
+    for word in file:
+        cipher = functions.shuffle_letters(word)
+        print(f"Угадай слово: {cipher}")
+        answer = input()
+        if answer.lower() == word.replace("\n", ""):
+            print("Верно! Вы получаете 10 баллов\n")
+            score += 10
+        else:
+            print(f"Неверно! Верный ответ - {word}")
+
+functions.write_to_the_top(name, score)
+functions.print_statistic()
+
+
+
+
