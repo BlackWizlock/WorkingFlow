ed543153335e2eb9c33ffed59d9d4d81ad09ed72
aa.salikov@mail.ru <!JQkSr5a>
2022-05-09 18:44:27 +0300

    Commit here

---
+++ WorkingFlow/Flask_HW_1/main.py
@@ -0,0 +1,65 @@
+import json
+from flask import Flask
+
+# глобальная переменная пути к БД
+DATABASE_FILE_PATH = r"candidates.json"
+
+
+def load_students(filename: str = DATABASE_FILE_PATH) -> list:
+    """
+    Загружает студентов из файла в список
+    Args:
+        filename: Имя файла JSON
+    Returns: возврат словаря JSON
+    """
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        return json.load(f)
+
+
+app = Flask(__name__)  # создаем экземпляр класса Фласк
+user_database = load_students()  # инициализация БД
+
+
+@app.route('/')
+def page_index():
+    """
+    Основная страничка
+    """
+    output = ""
+    for line in user_database:
+        output += line["name"] + "<br>" + line["position"] + "<br>" + line["skills"] + "<br><br>"
+    return "<pre>" + output + "</pre>"
+
+
+@app.route('/candidates/<string:x>')
+def page_candidates(x: str):
+    """
+    Личная страничка кандидата
+    Args:
+        x: Имя кандидата из БД
+    """
+    output = ""
+    for line in user_database:
+        if line["name"].lower() == x.lower():
+            output += "<img src=" + line["picture"] + "><br><br><pre>" + line["name"] + "<br>" + line[
+                "position"] + "<br>" + line["skills"] + "<br><br>"
+            break
+    return output + "</pre>"
+
+
+@app.route('/skills/<string:x>')
+def page_skills(x: str):
+    """
+    Страничка по скиллу
+    Args:
+        x: скилл для поиска в БД
+    """
+    output = ""
+    for line in user_database:
+        if x.lower() in line["skills"].lower():
+            output += line["name"] + "<br>" + line["position"] + "<br>" + line["skills"] + "<br><br>"
+    return "<pre>" + output + "</pre>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- WorkingFlow/HW7/functions.py
+++ WorkingFlow/HW7/functions.py
@@ -2,72 +2,72 @@ import json
 
 
 def load_students(filename: str) -> list:
-	'''
-	Загружает студентов из файла в список
-	:param
-	filename: Имя файла JSON
-	:return:
-	возврат словаря JSON
-	'''
-	with open(filename, 'r', encoding='utf-8-sig') as f:
-		return json.load(f)
+    """
+    Загружает студентов из файла в список
+    :param
+    filename: Имя файла JSON
+    :return:
+    возврат словаря JSON
+    """
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        return json.load(f)
 
 
 def load_professions(filename: str) -> list:
-	'''
-	Загружает навыки из файла в список
-	:param
-	filename: Имя файла JSON
-	:return:
-	возврат словаря JSON
-	'''
-	with open(filename, 'r', encoding='utf-8-sig') as f:
-		return json.load(f)
+    """
+    Загружает навыки из файла в список
+    :param
+    filename: Имя файла JSON
+    :return:
+    возврат словаря JSON
+    """
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        return json.load(f)
 
 
 def get_student_by_pk(pk: int, usr_lst: list):
-	'''
-	Получает словарь с данными студента по его pk
-	:param
-	pk: порядковый номер - ввод с клавиатуры
-	usr_lst: словарь JSON
-	:return:
-	Возврат строки словаря или False для отработки исключений базы
-	'''
-	for item in usr_lst:
-		if item['pk'] == pk:
-			return item
-	return False
+    """
+    Получает словарь с данными студента по его pk
+    :param
+    pk: порядковый номер - ввод с клавиатуры
+    usr_lst: словарь JSON
+    :return:
+    Возврат строки словаря или False для отработки исключений базы
+    """
+    for item in usr_lst:
+        if item['pk'] == pk:
+            return item
+    return False
 
 
 def get_profession_by_title(title: str, usr_lst: list):
-	'''
-	Получает словарь с инфо о профессии по названию
-	:param
-	title: профессия - ввод с клавиатуры
-	usr_lst: словарь JSON
-	:return:
-	Возврат строки словаря или False для отработки исключений базы
-	'''
-	for item in usr_lst:
-		if item['title'] == title:
-			return item
-	return False
+    """
+    Получает словарь с инфо о профессии по названию
+    :param
+    title: профессия - ввод с клавиатуры
+    usr_lst: словарь JSON
+    :return:
+    Возврат строки словаря или False для отработки исключений базы
+    """
+    for item in usr_lst:
+        if item['title'] == title:
+            return item
+    return False
 
 
 def check_fitness(student: set, profession: set) -> dict:
-	'''
-	Обработка множеств
-	:param
-	student: множество с информацией по знаниям студента
-	profession: множество с информацией по требованию работодателя
-	:return:
-	возврат словаря с отработкой множеств и сформированной карточкой соискателя
-	'''
-	out_dict = {
-			"has":         [*student.intersection(profession)],
-			"lacks":       [*profession.difference(student)],
-			"fit_percent": round(len(student.intersection(profession)) / len(profession) * 100)
-	}
+    """
+    Обработка множеств
+    :param
+    student: множество с информацией по знаниям студента
+    profession: множество с информацией по требованию работодателя
+    :return:
+    возврат словаря с отработкой множеств и сформированной карточкой соискателя
+    """
+    out_dict = {
+        "has": [*student.intersection(profession)],
+        "lacks": [*profession.difference(student)],
+        "fit_percent": round(len(student.intersection(profession)) / len(profession) * 100)
+    }
 
-	return out_dict
+    return out_dict
